aac8fce004c39d70a0cc34f0fdf10558c345c41a
Kodai Hayashi <hayashikdi0105@gmail.com>
2022-08-06 22:20:17 +0900

    plaintextをn個分ずらすアルゴリズムを作成した

--- caesar.c
+++ caesar.c
@@ -1,15 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-
+// char* caesar(char* text);
 
 int main(int argc, char* argv[])
 {
     if (argc == 2)
     {
-        printf("My name is %s", argv[1]);
+        int len = strlen(argv[1]);
+        for (int i = 0; i < len; i++)
+        {
+            if (argv[1][i]<'0' || argv[1][i]>'9')
+            {
+                printf("Usage: ./caesar key");
+                return 1;
+            }
+        }
     }
     else
     {
-        printf("Please try it again");
+        printf("Usage: ./caesar key");
+        return 1;
+    } 
+
+    char* plaintext;
+    printf("Plaintext: ");
+    scanf("%s", plaintext);
+
+    int n = atoi(argv[1]);
+
+    //plaintextをn個分ずらす
+    int len = strlen(plaintext);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (islower(plaintext[i]))
+        {
+            if (plaintext[i] + n > 'z') plaintext[i] = 97 + (plaintext[i] + n - 97)%26;
+            else plaintext[i] += n;
+        }
+        else
+        {
+            if (plaintext[i] + n > 'Z') plaintext[i] = 65 + (plaintext[i] + n - 65)%26;
+            else plaintext[i] += n;
+        }
     }
+    
+    printf("%s %i\n", plaintext, n);
+
+    return 0;
 }
+
+// char* caesar(char* text)
+// {
+//     return text;
+// }
